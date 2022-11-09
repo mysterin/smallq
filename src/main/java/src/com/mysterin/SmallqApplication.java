865aa217615d986ad82b1e28004f4fbc91190f5d
xiaobin.lin <xiaobin.lin@qlchat.com>
2022-11-09 19:00:32 +0800

    创建项目

---
+++ src/main/java/src/com/mysterin/SmallqApplication.java
@@ -0,0 +1,13 @@
+package com.mysterin;
+
+/**
+ * @author linxiaobin
+ * @Description
+ * @date 2022/11/9 19:00
+ */
+public class SmallqApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
